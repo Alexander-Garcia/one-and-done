7540a8e6eaf166077c7ec7f989ebfdb51471bacd
Alexander <31865557+Alexander-Garcia@users.noreply.github.com>
2023-12-26 20:28:20 -0600

    feat(app): dockerize whole app

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# one-and-done
+# One-And-Done
 NBA Finals pick 'em game for friends
+
+# Front End
+Created using Vite and TS
+
+# Back End
+Uses FastApi for python and utilizes: 
+* [FastAPI](https://fastapi.tiangolo.com/)
+* [NBA API](https://github.com/swar/nba_api/tree/master)
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import axios from 'axios';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const handleClick = async () => {
+    const data: string = await axios.get('//localhost:8000');
+    console.log('testing hmr');
+    console.log('data', data);
+    
+  };
 
   return (
     <>
       <div>
+        <h1>Testing</h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -18,8 +24,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={handleClick}>
+          count is
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
