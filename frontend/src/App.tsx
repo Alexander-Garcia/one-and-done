64004850becd7b24e5fefe2d79691a1c5a84d41b
Alexander <31865557+Alexander-Garcia@users.noreply.github.com>
2023-12-27 19:41:44 -0600

    feat(frontend): change svg, tab title, add material ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,24 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import axios from 'axios';
+import api from './api';
+import Header from './components/Header';
 import './App.css'
 
 function App() {
   const handleClick = async () => {
-    const data: string = await axios.get('//localhost:8000');
-    console.log('testing hmr');
+    const data: string = await api.get('/scoreboard');
     console.log('data', data);
-    
   };
 
   return (
     <>
+      <Header />
       <div>
         <h1>testing</h1>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={handleClick}>
           count is
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
